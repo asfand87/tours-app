f5718cb55af5b993ddcb93c6667c79b29895d5b3
asfand87 <asfand1987@gmail.com>
2022-09-26 20:24:52 +0100

    display_items changed to line_items.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -16,7 +16,7 @@ const checkoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: tourID,
-    display_items: [{
+    line_items: [{
       name: `${tour.name} Tour`,
       description: tour.summary,
       images: [`${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`],
@@ -37,7 +37,7 @@ const checkoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckOut = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 }
 
