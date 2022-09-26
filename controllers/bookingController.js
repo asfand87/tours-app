a4ea1c3395b61a643f9db72c47187e15f2a9d08a
asfand87 <asfand1987@gmail.com>
2022-09-26 23:46:44 +0100

    minor changes to booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,20 +11,26 @@ const checkoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(tourID);
   // 2) Create checkout session.
   const session = await stripe.checkout.sessions.create({
+    mode: "payment",
     payment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: tourID,
-    line_items: [{
-      name: `${tour.name} Tour`,
-      description: tour.summary,
-      images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover
-        }`],
-      amount: tour.price * 100,
-      currency: 'usd',
-      quantity: 1
-    }]
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
   })
   // 3) Create session as response.
   res.status(200).json({
@@ -37,8 +43,8 @@ const checkoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckOut = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  const price = session.amount_total / 100;
   const data = { tour: tour, user: user, price: price };
   await Booking.create(data);
 }
